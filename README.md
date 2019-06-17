feef6e1c59e9d86f26ed376c614520e22e225c23
Michael A. Chapman <chapmanm305@gmail.com>
2019-06-17 16:46:11 -0700

    Update readme and generate documentation

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 * [submission PR](https://github.com/michaelchapman-401-advanced-javascript/lab-16/pull/1)
 * [travis](https://travis-ci.org/michaelchapman-401-advanced-javascript/lab-16)
 * [UML](./assets/whiteboard.jpg)
-* [jsdoc]()
+* [jsdoc](./docs/index.html)
 
 ### Modules
 Implement events in Node
--- events/logger.js
+++ events/logger.js
@@ -2,6 +2,11 @@
 
 const events = require('./event.js');
 
+/**
+ * @module log
+ * @param {string} file - Name of the file to read from
+ * @desc Handles console logging the save message
+ */
 function log(file) {
   if (typeof file !== 'string') { throw new Error('Not a valid string'); }
   console.log(`${file} saved`);
